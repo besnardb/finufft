2d4a7f43dc159cdca9f425b9234e10fb9f118962
Baptiste Besnard <Baptiste.BESNARD@obspm.fr>
2026-07-24 17:33:51 +0200

    update exception

--- include/cufinufft/contrib/helper_hip.h
+++ include/cufinufft/contrib/helper_hip.h
@@ -29,11 +29,61 @@
  */
 
 #include <string>
+#include <hipfft/hipfft.h>
 #include <hip/hip_runtime.h>
 #include <finufft_common/safe_call.h>
 #include <finufft_errors.h>
 
 namespace cufinufft {
+
+inline const char *hipfftGetErrorString(hipfftResult error) {
+  switch (error) {
+  case HIPFFT_SUCCESS:
+    return "HIPFFT_SUCCESS";
+
+  case HIPFFT_INVALID_PLAN:
+    return "HIPFFT_INVALID_PLAN";
+
+  case HIPFFT_ALLOC_FAILED:
+    return "HIPFFT_ALLOC_FAILED";
+
+  case HIPFFT_INVALID_TYPE:
+    return "HIPFFT_INVALID_TYPE";
+
+  case HIPFFT_INVALID_VALUE:
+    return "HIPFFT_INVALID_VALUE";
+
+  case HIPFFT_INTERNAL_ERROR:
+    return "HIPFFT_INTERNAL_ERROR";
+
+  case HIPFFT_EXEC_FAILED:
+    return "HIPFFT_EXEC_FAILED";
+
+  case HIPFFT_SETUP_FAILED:
+    return "HIPFFT_SETUP_FAILED";
+
+  case HIPFFT_INVALID_SIZE:
+    return "HIPFFT_INVALID_SIZE";
+
+  case HIPFFT_UNALIGNED_DATA:
+    return "HIPFFT_UNALIGNED_DATA";
+
+  case HIPFFT_INVALID_DEVICE:
+    return "HIPFFT_INVALID_DEVICE";
+
+  case HIPFFT_NO_WORKSPACE:
+    return "HIPFFT_NO_WORKSPACE";
+
+  case HIPFFT_NOT_IMPLEMENTED:
+    return "HIPFFT_NOT_IMPLEMENTED";
+
+  case HIPFFT_NOT_SUPPORTED:
+    return "HIPFFT_NOT_SUPPORTED";
+
+  default:
+    return "<unknown>";
+  }
+}
 
 // Typed exception carrying a hipError_t plus a caller-supplied operation tag
 // and optional source location.
@@ -72,7 +122,7 @@ public:
                    const char *op,
                    const char *file = nullptr,
                    int line = 0)
-      : finufft::exception(FINUFFT_ERR_CUDA_FAILURE,
+      : finufft::exception(FINUFFT_ERR_HIP_FAILURE,
                            format(err, op, file, line)),
         hipfft_code_(err) {}
 
